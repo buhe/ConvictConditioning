fb3e52350367f09fb693db792f3a5cfdeacbb848
buhe <bugu1986@126.com>
2023-06-25 15:16:41 +0800

    null

--- Convict Conditioning Pro/ContentView.swift
+++ Convict Conditioning Pro/ContentView.swift
@@ -88,23 +88,59 @@ struct ContentView: View {
 //        let c = loader.load()
 //        print(c)
         
-        let loader = TextLoader(file_path: "state_of_the_union.txt")
-        let documents = loader.load()
-        let text_splitter = CharacterTextSplitter(chunk_size: 1000, chunk_overlap: 0)
- 
-        let embeddings = OpenAIEmbeddings()
-        let s = Supabase(embeddings: embeddings)
+//        let loader = TextLoader(file_path: "state_of_the_union.txt")
+//        let documents = loader.load()
+//        let text_splitter = CharacterTextSplitter(chunk_size: 1000, chunk_overlap: 0)
+// 
+//        let embeddings = OpenAIEmbeddings()
+//        let s = Supabase(embeddings: embeddings)
+//        Task {
+//            for text in documents {
+//                let docs = text_splitter.split_text(text: text.page_content)
+//                for doc in docs {
+//                    await s.addText(text: doc)
+//                }
+//            }
+//            
+//            let m = await s.similaritySearch(query: "What did the president say about Ketanji Brown Jackson", k: 1)
+//            print("Q:What did the president say about Ketanji Brown Jackson")
+//            print("A:\(m)")
+//        }
+        
+        
+        let template = """
+        Assistant is a large language model trained by OpenAI.
+
+        Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+
+        Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+        Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
+
+        %@
+        Human: %@
+        Assistant:
+"""
+
+        let prompt = PromptTemplate(input_variables: ["history", "human_input"], template: template)
+
+
+        let chatgpt_chain = LLMChain(
+            llm: OpenAI(),
+            prompt: prompt,
+            parser: Nothing(),
+            memory: ConversationBufferWindowMemory()
+        )
         Task {
-            for text in documents {
-                let docs = text_splitter.split_text(text: text.page_content)
-                for doc in docs {
-                    await s.addText(text: doc)
-                }
-            }
-            
-            let m = await s.similaritySearch(query: "What did the president say about Ketanji Brown Jackson", k: 1)
-            print("Q:What did the president say about Ketanji Brown Jackson")
-            print("A:\(m)")
+            var input = ["human_input": "I want you to act as a Linux terminal. I will type commands and you will reply with what the terminal should show. I want you to only reply with the terminal output inside one unique code block, and nothing else. Do not write explanations. Do not type commands unless I instruct you to do so. When I need to tell you something in English I will do so by putting text inside curly brackets {like this}. My first command is pwd."
+            ]
+            var output = await chatgpt_chain.predict(args: input)
+            print(input["human_input"]!)
+            print(output["Answer"]!)
+            input = ["human_input": "ls ~"]
+            output = await chatgpt_chain.predict(args: input)
+            print(input["human_input"]!)
+            print(output["Answer"]!)
         }
     }
     var body: some View {
